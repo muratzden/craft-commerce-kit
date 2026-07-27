f31ee9fd34799a8906af124529221402bb89f09b
Murat Ozden <muratzden@gmail.com>
2026-07-27 17:40:32 +0300

    feat: improve USP block editing

--- blocks/usp/editor.js
+++ blocks/usp/editor.js
@@ -4,6 +4,7 @@
     var el = element.createElement;
     var useBlockProps = blockEditor.useBlockProps;
     var InspectorControls = blockEditor.InspectorControls;
+    var RichText = blockEditor.RichText;
     var PanelBody = components.PanelBody;
     var renderControl = window.cckBlockEditor.renderControl;
     var editorSettings = (
@@ -67,10 +68,10 @@
                     el(
                         'div',
                         { className: 'cck-container cck-usp-grid' },
-                        [
-                            ['item_one_title', 'item_one_text'],
-                            ['item_two_title', 'item_two_text'],
-                            ['item_three_title', 'item_three_text']
+                       [
+                            ['item_one_icon', 'item_one_title', 'item_one_text'],
+                            ['item_two_icon', 'item_two_title', 'item_two_text'],
+                            ['item_three_icon', 'item_three_title', 'item_three_text']
                         ].map(function (keys, index) {
                             return el(
                                 'article',
@@ -78,12 +79,35 @@
                                     className: 'cck-usp-item',
                                     key: index
                                 },
-                                attributes[keys[0]]
-                                    ? el('h3', null, attributes[keys[0]])
-                                    : null,
-                                attributes[keys[1]]
-                                    ? el('p', null, attributes[keys[1]])
-                                    : null
+                                el(
+                                    'span',
+                                    {
+                                        className: 'dashicons dashicons-' + (attributes[keys[0]] || 'star-filled') + ' cck-usp-item__icon',
+                                        'aria-hidden': 'true'
+                                    }
+                                ),
+                               el(RichText, {
+                                    tagName: 'h3',
+                                    value: attributes[keys[1]],
+                                    allowedFormats: [],
+                                    placeholder: 'USP title…',
+                                    onChange: function (value) {
+                                        var update = {};
+                                        update[keys[1]] = value;
+                                        setAttributes(update);
+                                    }
+                                }),
+                                el(RichText, {
+                                    tagName: 'p',
+                                    value: attributes[keys[2]],
+                                    allowedFormats: [],
+                                    placeholder: 'USP text…',
+                                    onChange: function (value) {
+                                        var update = {};
+                                        update[keys[2]] = value;
+                                        setAttributes(update);
+                                    }
+                                })
                             );
                         })
                     )
