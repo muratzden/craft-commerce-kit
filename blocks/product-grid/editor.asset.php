bf1d677be044ef618d36ace0594ae807d35909b4
Murat Ozden <muratzden@gmail.com>
2026-07-27 22:30:25 +0300

    feat: add Product Grid editor assets

---
+++ blocks/product-grid/editor.asset.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+        'dependencies' => array(
+                'wp-blocks',
+                'wp-element',
+                'wp-block-editor',
+                'wp-components',
+                'wp-server-side-render',
+                'cck-block-editor-controls',
+        ),
+        'version'      => CCK_VERSION,
+);
