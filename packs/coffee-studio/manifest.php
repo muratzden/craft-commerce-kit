f42b405bb4b303ac53808110cc9f57112857f8fd
Murat Ozden <muratzden@gmail.com>
2026-07-28 19:20:34 +0300

    feat: add Coffee Studio experience manifest

---
+++ packs/coffee-studio/manifest.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Coffee Studio Experience Manifest.
+ *
+ * @package CraftCommerceKit
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+return array(
+	'id'          => 'coffee-studio',
+	'name'        => 'Coffee Studio',
+	'version'     => '1.0.0',
+	'author'      => 'Craft Commerce Kit',
+	'description' => 'Premium commerce experience for independent coffee roasters and cafes.',
+	'supports'    => array(
+		'brand-presets',
+		'components',
+		'layouts',
+		'templates',
+		'tokens',
+		'woocommerce',
+	),
+);
