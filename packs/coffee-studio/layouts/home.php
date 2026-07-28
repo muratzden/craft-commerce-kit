fe782f56ff2d8e69f8af2dc5f8f49da5a820336f
Murat Ozden <muratzden@gmail.com>
2026-07-28 19:28:38 +0300

    feat: add Coffee Studio home layout

---
+++ packs/coffee-studio/layouts/home.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Coffee Studio home layout.
+ *
+ * @package CraftCommerceKit
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+return array(
+	'hero',
+	'intro',
+	'product-grid',
+	'story',
+	'collections',
+	'trust',
+	'cta',
+	'footer',
+);
