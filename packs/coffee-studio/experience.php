f90dd3e2fe7babcf5712070daa5994f68f2ff31f
Murat Ozden <muratzden@gmail.com>
2026-07-28 19:25:51 +0300

    feat: add Coffee Studio experience definition

---
+++ packs/coffee-studio/experience.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Coffee Studio Experience definition.
+ *
+ * @package CraftCommerceKit
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+return array(
+	'id'     => 'coffee-studio',
+	'name'   => 'Coffee Studio',
+	'layout' => 'home',
+);
