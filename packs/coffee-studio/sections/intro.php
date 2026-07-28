d95e997ee74717cc9b6b9a03c87abf6fdf0ee882
Murat Ozden <muratzden@gmail.com>
2026-07-28 19:36:45 +0300

    feat: add Coffee Studio intro section

---
+++ packs/coffee-studio/sections/intro.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Coffee Studio intro section.
+ *
+ * @package CraftCommerceKit
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+return array(
+	'component'  => 'section-title',
+	'attributes' => array(
+		'eyebrow' => 'Selected coffees',
+		'title'   => 'Roasted with clarity, chosen for character.',
+		'text'    => 'A focused collection of seasonal coffees selected to make origin, process, and flavour easy to understand.',
+		'align'   => 'left',
+	),
+);
